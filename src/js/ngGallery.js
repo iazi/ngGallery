ef16aec5722ceb7d2b17aa0bbb3db85e39e4542d
Fabian Bernhard <fabian@veeting.com>
2016-05-10 09:40:11 +0200

    angular-file-upload component added

--- src/js/ngGallery.js
+++ src/js/ngGallery.js
@@ -1,11 +1,13 @@
 (function () {
     'use-strict';
 
-    angular.module('jkuri.gallery', []).directive('ngGallery', ngGallery);
+    angular.module('jkuri.gallery', ['angularFileUpload'])
+      .directive('ngGallery', ngGallery)
+      .directive('ngThumb', ngThumb);
 
-    ngGallery.$inject = ['$document', '$timeout', '$q', '$templateCache'];
+    ngGallery.$inject = ['$document', '$timeout', '$q', '$templateCache', 'FileUploader'];
 
-    function ngGallery($document, $timeout, $q, $templateCache) {
+    function ngGallery($document, $timeout, $q, $templateCache, FileUploader) {
 
         var defaults = {
             baseClass: 'ng-gallery',
@@ -38,7 +40,7 @@
             alt="Image {{ $index + 1 }}" />
         </div>
         <div
-          ng-if="addMore"
+          ng-if="uploadCallback"
           class="ng-gallery-add">
           <i
             class="fa fa-plus"
@@ -102,18 +104,129 @@
       </div>
       <div
         unselectable="on"
-        class="ng-gallery-content"
+        class="ng-gallery-file-upload-form"
         ng-show="addMoreOpened">
-        <a
-          class="close-popup"
-          ng-click="closeAddImageDialog()">
-          <i class="fa fa-close"></i>
-        </a>  
-        <form>
-          <input type="file" multiple accept='image/*' />
-          <button type="button">Add selected images</button>
-        </form>
-      </div>
+        <div class="row">
+          <div class="col-md-3"></div>
+          <div class="col-md-6 ng-gallery-file-upload-control">
+            <a
+              class="close-popup pull-right"
+              ng-click="closeAddImageDialog()">
+              <i class="fa fa-close"></i>
+            </a>
+            <div>
+              <h3>Upload images</h3>
+              <!--
+                <div ng-show="uploader.isHTML5">
+                  <div
+                    class="well my-drop-zone"
+                    nv-file-over=""
+                    uploader="uploader">Drop images here</div>
+                </div>
+                -->
+              <input
+                type="file"
+                nv-file-select=""
+                uploader="uploader"
+                filter="images/*"
+                multiple /><br />
+            </div>
+            <div>
+              <table class="table">
+                <thead>
+                  <tr>
+                    <th width="50%">Name</th>
+                    <th ng-show="uploader.isHTML5">Size</th>
+                    <th ng-show="uploader.isHTML5">Progress</th>
+                    <th>Status</th>
+                    <th>Actions</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  <tr ng-repeat="item in uploader.queue">
+                    <td><strong>{{ item.file.name }}</strong> <!-- Image preview --> <!--auto height--> <!--<div ng-thumb="{ file: item.file, width: 100 }"></div>-->
+                      <!--auto width-->
+                      <div
+                        ng-show="uploader.isHTML5"
+                        ng-thumb="{ file: item._file, height: 100 }"></div> <!--fixed width and height --> <!--<div ng-thumb="{ file: item.file, width: 100, height: 100 }"></div>-->
+                    </td>
+                    <td
+                      ng-show="uploader.isHTML5"
+                      nowrap>{{ item.file.size/1024/1024|number:2 }} MB</td>
+                    <td ng-show="uploader.isHTML5">
+                      <div
+                        class="progress"
+                        style="margin-bottom: 0;">
+                        <div
+                          class="progress-bar"
+                          role="progressbar"
+                          ng-style="{ 'width': item.progress + '%' }"></div>
+                      </div>
+                    </td>
+                    <td class="text-center"><span ng-show="item.isSuccess"><i class="glyphicon glyphicon-ok"></i></span> <span ng-show="item.isCancel"><i
+                          class="glyphicon glyphicon-ban-circle"></i></span> <span ng-show="item.isError"><i class="glyphicon glyphicon-remove"></i></span></td>
+                    <td nowrap>
+                      <button
+                        type="button"
+                        class="btn btn-success btn-xs"
+                        ng-click="item.upload()"
+                        ng-disabled="item.isReady || item.isUploading || item.isSuccess">
+                        <span class="glyphicon glyphicon-upload"></span> Upload
+                      </button>
+                      <button
+                        type="button"
+                        class="btn btn-warning btn-xs"
+                        ng-click="item.cancel()"
+                        ng-disabled="!item.isUploading">
+                        <span class="glyphicon glyphicon-ban-circle"></span> Cancel
+                      </button>
+                      <button
+                        type="button"
+                        class="btn btn-danger btn-xs"
+                        ng-click="item.remove()">
+                        <span class="glyphicon glyphicon-trash"></span> Remove
+                      </button>
+                    </td>
+                  </tr>
+                </tbody>
+              </table>
+              <div>
+                <div>
+                  Queue progress:
+                  <div
+                    class="progress"
+                    style="">
+                    <div
+                      class="progress-bar"
+                      role="progressbar"
+                      ng-style="{ 'width': uploader.progress + '%' }"></div>
+                  </div>
+                </div>
+                <button
+                  type="button"
+                  class="btn btn-success btn-s"
+                  ng-click="uploader.uploadAll()"
+                  ng-disabled="!uploader.getNotUploadedItems().length">
+                  <span class="glyphicon glyphicon-upload"></span> Upload all
+                </button>
+                <button
+                  type="button"
+                  class="btn btn-warning btn-s"
+                  ng-click="uploader.cancelAll()"
+                  ng-disabled="!uploader.isUploading">
+                  <span class="glyphicon glyphicon-ban-circle"></span> Cancel all
+                </button>
+                <button
+                  type="button"
+                  class="btn btn-danger btn-s"
+                  ng-click="uploader.clearQueue()"
+                  ng-disabled="!uploader.queue.length">
+                  <span class="glyphicon glyphicon-trash"></span> Remove all
+                </button>
+              </div>
+            </div>
+          </div>
+        </div>
         `;
 
         $templateCache.put(template_url, template);
@@ -124,7 +237,8 @@
                 images: '=',
                 thumbsNum: '@',
                 hideOverflow: '=',
-                addMore: "="
+                uploadCallback: "=",
+                uploadUrl: "="
             },
             controller: [
                 '$scope',
@@ -134,6 +248,17 @@
                     });
                     $scope.$on('addImage', function (e, args) {
                       $scope.addImage();
+                    });
+                    $scope.uploader = new FileUploader({
+                      url: $scope.uploadUrl
+                    });
+
+                    $scope.uploader.filters.push({
+                        name: 'imageFilter',
+                        fn: function(item /*{File|FileLikeObject}*/, options) {
+                            var type = '|' + item.type.slice(item.type.lastIndexOf('/') + 1) + '|';
+                            return '|jpg|png|jpeg|bmp|gif|'.indexOf(type.toLowerCase()) !== -1;
+                        }
                     });
                 }
             ],
@@ -225,8 +350,15 @@
                   scope.addMoreOpened = true;
                   if (scope.hideOverflow) {
                     $('body').css({overflow: 'hidden'});
-                  }                  
+                  }   
                 };
+                
+                scope.uploadFiles = function() {
+                  if (scope.uploadCallback) {
+                    scope.uploadCallback();
+                  }
+                  scope.addMoreOpened = false;
+                }
                 
                 scope.closeAddImageDialog = function () {
                   scope.addMoreOpened = false;
@@ -309,4 +441,50 @@
             }
         };
     }
+    
+    ngThumb.$inject = ['$window'];
+    function ngThumb($window) {
+      var helper = {
+          support: !!($window.FileReader && $window.CanvasRenderingContext2D),
+          isFile: function(item) {
+              return angular.isObject(item) && item instanceof $window.File;
+          },
+          isImage: function(file) {
+              var type =  '|' + file.type.slice(file.type.lastIndexOf('/') + 1) + '|';
+              return '|jpg|png|jpeg|bmp|gif|'.indexOf(type) !== -1;
+          }
+      };
+
+      return {
+          restrict: 'A',
+          template: '<canvas/>',
+          link: function(scope, element, attributes) {
+              if (!helper.support) return;
+
+              var params = scope.$eval(attributes.ngThumb);
+
+              if (!helper.isFile(params.file)) return;
+              if (!helper.isImage(params.file)) return;
+
+              var canvas = element.find('canvas');
+              var reader = new FileReader();
+
+              reader.onload = onLoadFile;
+              reader.readAsDataURL(params.file);
+
+              function onLoadFile(event) {
+                  var img = new Image();
+                  img.onload = onLoadImage;
+                  img.src = event.target.result;
+              }
+
+              function onLoadImage() {
+                  var width = params.width || this.width / this.height * params.height;
+                  var height = params.height || this.height / this.width * params.width;
+                  canvas.attr({ width: width, height: height });
+                  canvas[0].getContext('2d').drawImage(this, 0, 0, width, height);
+              }
+          }
+      };
+  }
 })();
